4be5a4f2978d079e94d998b9489584662a81237e
dev.m.butylkin <oldqug@yandex.ru>
2025-02-11 18:56:22 +0400

    Реализована часть бизнес логики сокращения ссылок

--- internal/providers/memory_provider/provider.go
+++ internal/providers/memory_provider/provider.go
@@ -2,29 +2,29 @@ package memory_provider
 
 import "fmt"
 
-type memoryProvider interface {
+type memoryAdapter interface {
 	GetValueMemory(memoryKeyId string) (string, error)
 	SetValueMemory(key string, value string)
 }
 
-// memoryProvider - провайдер для взаимодействия с memoryAdapter.
-type provider struct {
-	ma memoryProvider
+// MemoryProvider - провайдер для взаимодействия с memoryAdapter.
+type MemoryProvider struct {
+	ma memoryAdapter
 }
 
-func New(ma memoryProvider) *provider {
-	return &provider{
+func New(ma memoryAdapter) *MemoryProvider {
+	return &MemoryProvider{
 		ma: ma,
 	}
 }
 
-func (p *provider) GetValueMemory(memoryKeyId string) (string, error) {
+func (p *MemoryProvider) GetValueMemory(memoryKeyId string) (string, error) {
 	value, err := p.ma.GetValueMemory(memoryKeyId)
 	if err != nil {
 		return "", fmt.Errorf("не удалось найти ключ: %w", err)
 	}
 	return value, nil
 }
-func (p *provider) SetValueMemory(key string, value string) {
+func (p *MemoryProvider) SetValueMemory(key string, value string) {
 	p.ma.SetValueMemory(key, value)
 }
