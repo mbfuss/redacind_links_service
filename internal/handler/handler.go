9e9c2a6750b0d5cef6fe776a2fc9430483a86ccd
dev.m.butylkin <oldqug@yandex.ru>
2025-02-11 16:04:52 +0400

    Реализованы memory адаптер и провайдер

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -2,7 +2,10 @@ package handler
 
 import "net/http"
 
-func RegisterHandlers(mux *http.ServeMux) {
+type Handler struct {
+}
+
+func (h *Handler) RegisterHandlers(mux *http.ServeMux) {
 	mux.HandleFunc("/", nil)
 	mux.HandleFunc("/{id}", nil)
 }
